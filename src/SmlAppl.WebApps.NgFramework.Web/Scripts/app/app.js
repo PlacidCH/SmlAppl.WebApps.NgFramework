90a92fc58e12e48f493ba581bfcb2b8c95a97734
Shahid Schmid <shahid.schmid@premotec.ch>
2016-01-06 22:34:30 +0100

    Initial

---
+++ src/SmlAppl.WebApps.NgFramework.Web/Scripts/app/app.js
@@ -0,0 +1,22 @@
+﻿(function() {
+	'use strict';
+
+	angular.module("SmlAppl.WebApps.Framework.Controllers", []);
+	angular.module("SmlAppl.WebApps.Framework.Directives", []);
+	angular.module("SmlAppl.WebApps.Framework.Filters", []);
+	angular.module("SmlAppl.WebApps.Framework.Services", []);
+
+	angular.module('SmlAppl.WebApps.Framework', [
+		// Angular modules 
+		//'ngRoute'
+		//"ui.router",
+
+		// Custom modules 
+		"SmlAppl.WebApps.Framework.Controllers",
+		"SmlAppl.WebApps.Framework.Directives",
+		"SmlAppl.WebApps.Framework.Filters",
+		"SmlAppl.WebApps.Framework.Services",
+
+		// 3rd Party Modules
+	]);
+})();
