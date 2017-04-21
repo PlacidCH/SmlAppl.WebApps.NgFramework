12a31112378138607077a16d2a89c41aca47b892
Hesyar Uzuner <hesyar.uzuner@premotec.ch>
2017-04-21 10:16:49 +0200

    Removed LoginCtrl from framework

--- dist/js/SmlAppl.WebApps.NgFramework.js
+++ dist/js/SmlAppl.WebApps.NgFramework.js
@@ -381,55 +381,6 @@ angular.module('smlAppl.webApps.framework').run(['$templateCache', function($tem
 
 })();
 
-/* #### File: ./src/Scripts/app/Controllers/LoginCtrl.js */ 
-/*
- * When data.redirectState is defined on the state, this state will be used to redirect.
- * Otherwise a redirect to home will take place.
- */
-
-(function () {
-	"use strict";
-
-	angular.module("smlAppl.webApps.framework.controllers")
-		.controller("LoginCtrl", [
-			"$scope", "$state", "Authentication", "$rootScope", "$location",
-			function($scope, $state, Authentication, $rootScope, $location) {
-				$scope.loginData = {
-					userName: "",
-					password: ""
-				};
-
-				$scope.errMessage = "";
-
-				var destPath = "";
-				if (localStorage.destinationPath && localStorage.destinationPath != "") {
-				    // Prepare redirection to the previously attempted url
-				    destPath = localStorage.destinationPath;
-				    localStorage.destinationPath = "";
-				}
-
-
-				$scope.login = function () {
-
-					Authentication.login($scope.loginData).then(function(response) {
-						// login successful
-					    if (destPath && destPath != "") {
-					        // Redirect to the previously attempted url
-					        $location.url(destPath);
-						} else {
-							// no redirect defined -> go to home
-							$state.go("home");
-                        }
-					},
-					function(err) {
-						$scope.errMessage = err.error_description;
-					});
-				};
-			}
-		]);
-
-})();
-
 /* #### File: ./src/Scripts/app/Controllers/MsgBoxCtrl.js */ 
 /*
 	The modal provides the params content and data.
